a1101dfa98abcde4829d71d6c1dd247c4adc26ee
kingderick55 <165064725+kingderick55@users.noreply.github.com>
2024-05-10 10:50:40 +0200

    latest changes

---
+++ js/hw.js
@@ -0,0 +1,72 @@
+// WHAT IS JS
+// HOW TO RUN JS
+// VARIABLES IN JS-(5 BASIC VARIABLES TYPES)
+// every variable needs to have a value if not it will be considered as undefined
+//Variables are Containers for Storing Data
+//JavaScript Variables can be declared in 4 ways:
+//Automatically
+// Using var
+// Using let
+// Using const
+
+ // 1. NUMBER TYPE
+
+ // using var to declare a number type variable
+ var x = 5;
+ var y = 6;
+ var sum = x + y;
+console.log( sum);
+
+// using let to declare a number type variable
+let a = 20;
+let b = 6;
+let c = a + b;
+console.log( a , b, c);
+
+// using const to declare a number type variable
+const price1 = 10;
+const price2 = 6;
+let total = price1 + price2;
+console.log( price1,price2,total);
+
+//2. BOOLEAN TYPE (ASK EXPLANATION FROM THE TEACHER)
+var isInBasket= true;
+// 3. strings and if conditional statements
+var message = "the ball is in the basket";
+if (isInBasket!=true) {
+    console.log(isInBasket);  
+}else if (isInBasket) {
+    console.log(message ,isInBasket);  
+}
+if (a==20){
+    console.log(a);
+}
+
+
+//4. functions
+//function declaration
+function addFunction(a,b){
+    console.log(a+b);
+} 
+//function calling
+addFunction(x,y);
+addFunction(price1,price2)
+
+// how to write and get content inside the html file
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// display my name on the comsole
+console.log("DERICK");
